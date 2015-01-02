c04631fe15af0f62b5f2db0a8aa2c820477f1f5e
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-02 11:53:19 -0800

    simple ascii art part 2

---
+++ simple_ascii_art/simple_ascii_art_2.rb
@@ -0,0 +1,15 @@
+puts "What character do you want to make the pyramid out of?"
+
+character = gets.chomp
+
+puts "How many rows of #{character}'s do you want?'"
+
+row = gets.chomp.to_i
+
+character_count = 1
+width = 100
+
+row.times do
+  puts ("#{character}" * character_count).center(width)
+  character_count +=2
+end
