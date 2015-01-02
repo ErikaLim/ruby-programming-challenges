ee890e8d6306158a40b066726b7b53bd0705d3d1
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-02 11:44:24 -0800

    ascii art part 1

--- odd_numbers/odd_numbers.rb
+++ odd_numbers/odd_numbers.rb
@@ -1,7 +1,7 @@
 # Write code to print all the odd numbers between 1 and 1337
 
 for x in 1..1337
-  if x%2 ==1
+  if x % 2 == 1
     puts x
   end
 end
---
+++ simple_ascii_art/simple_ascii_art_1.rb
@@ -0,0 +1,6 @@
+star = 1
+
+5.times do
+  puts ("*" * star).center(9)
+  star +=2
+end
