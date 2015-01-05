8e0cdb364128fbcde8d5e48bf2d7d01994a6de69
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-05 08:35:21 -0800

    added class

--- odd_numbers/odd_numbers.rb
+++ odd_numbers/odd_numbers.rb
@@ -1,7 +1,10 @@
 # Write code to print all the odd numbers between 1 and 1337
-
-for x in 1..1337
-  if x % 2 == 1
-    puts x
+class OddNumbers
+  for x in 1..1337
+    if x % 2 == 1
+      puts x
+    end
   end
 end
+
+OddNumbers.new
