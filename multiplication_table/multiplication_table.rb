401a4e6c87905112d344f800ce0f761bd60ac463
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-06 08:54:49 -0800

    fixed multiplication table

--- multiplication_table/multiplication_table.rb
+++ multiplication_table/multiplication_table.rb
@@ -3,17 +3,17 @@ puts "A multiplication table:"
 x = (1..9)
 y = (1..9)
 
-print '    '
+print '     '
 x.each do |x|
-  print "   #{x}"
+  print "   #{x}".rjust(5)
 end
-print "\n    "
+print "\n     "
 print "\n"
 
 y.each do |y|
   print "   #{y}|"
   x.each do |x|
-    print "   #{x*y}"
+    print "   #{x*y}".rjust(5)
   end
   print " \n"
 end
